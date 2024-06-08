e45a503959ced55212809f0cfcb0b4cf13fecd1e
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-06-08 16:18:53 +0530

    image card created

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,20 @@
 import React from "react";
-import img from "../assets/snapedit_1717738486483.png";
+import img from "../assets/image.svg";
 
 export default function About() {
   return (
     <div className="relative">
-      <div className="flex bg-gray-500 rounded-lg items-center justify-center ">
-        <div className="flex flex-col gap-4 text-4xl text-black items-center justify-center">
-          Hi I am Sakhi Rotliwala
-          <div className="text-black text-2xl">
+      <div className="flex flex-col-reverse mt-10 rounded-lg items-center justify-center md:flex-row">
+        <div className="rubik-title flex flex-col gap-4 text-4xl text-white text-left md:text-7xl ">
+          Hi, I am Sakhi Rotliwala
+          <div className="font-sans text-white text-lg font-normal text-left md:text-2xl">
             Passionate MERN stack developer skilled in MongoDB, Express.js,
-            React, and Node.js. I create dynamic, responsive web applications
-            with a strong focus on user experience and performance. With a keen
-            eye for detail and a commitment to clean code, I deliver innovative
-            solutions that meet client needs and exceed expectations.
+            React, and Node.js,creating dynamic, responsive web applications
+            with a user-focused approach.
           </div>
         </div>
         <div className="">
-          <img src={img} alt="" />
+          <img src={img} alt="image" />
         </div>
       </div>
     </div>
